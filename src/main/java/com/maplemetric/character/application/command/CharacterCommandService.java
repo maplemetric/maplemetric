cbd764f5260d51e6e6435128388345bceb020df0
jihoon <git4ub0@gmail.com>
2026-07-20 00:19:55 +0900

    feat: Character 모듈 기본 구조 생성

---
+++ src/main/java/com/maplemetric/character/application/command/CharacterCommandService.java
@@ -0,0 +1,9 @@
+package com.maplemetric.character.application.command;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CharacterCommandService {
+}
