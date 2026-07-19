f1fb6049331a9f5797ca5e42bb7b1d0d50dd5f33
jihoon <git4ub0@gmail.com>
2026-07-20 03:17:30 +0900

    feat: Nexon Open API 클라이언트 설정 추가

--- src/main/java/com/maplemetric/global/config/RestClientConfig.java
+++ src/main/java/com/maplemetric/global/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.maplemetric.global.config;
 
+import com.maplemetric.global.config.properties.NexonApiProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,8 +8,18 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
-    @Bean
-    public RestClient restClient() {
-        return RestClient.builder().build();
+    private static final String NEXON_API_KEY_HEADER = "x-nxopen-api-key";
+
+    @Bean("nexonRestClient")
+    public RestClient nexonRestClient(
+            NexonApiProperties nexonApiProperties
+    ) {
+        return RestClient.builder()
+                .baseUrl(nexonApiProperties.baseUrl())
+                .defaultHeader(
+                        NEXON_API_KEY_HEADER,
+                        nexonApiProperties.key()
+                )
+                .build();
     }
 }
