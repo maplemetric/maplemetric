0ffc2dbe4bc468ab672000ba51c0795e14e2d878
jihoon <git4ub0@gmail.com>
2026-07-19 22:41:53 +0900

    init commit

---
+++ src/main/java/com/maplemetric/MaplemetricServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maplemetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplemetricServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaplemetricServiceApplication.class, args);
+    }
+
+}
