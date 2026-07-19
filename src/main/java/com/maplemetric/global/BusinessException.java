43eb2ad71ebc3b5a7921e463c1e767678eb88787
jihoon <git4ub0@gmail.com>
2026-07-20 03:33:27 +0900

    feat: 공통 예외 및 실패 응답 구조 구현

---
+++ src/main/java/com/maplemetric/global/BusinessException.java
@@ -0,0 +1,14 @@
+package com.maplemetric.global;
+
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
---
+++ src/main/java/com/maplemetric/global/ErrorCode.java
@@ -0,0 +1,12 @@
+package com.maplemetric.global;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    HttpStatus getHttpStatus();
+
+    String getCode();
+
+    String getMessage();
+}
