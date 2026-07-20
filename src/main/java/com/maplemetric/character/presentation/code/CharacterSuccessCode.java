832d5887e539d48c3906946c3343a36f9cfd1ef7
jihoon <git4ub0@gmail.com>
2026-07-20 14:04:59 +0900

    feat: 캐릭터 기본 정보 조회 API 추가

--- src/main/java/com/maplemetric/character/presentation/code/CharacterSuccessCode.java
+++ src/main/java/com/maplemetric/character/presentation/code/CharacterSuccessCode.java
@@ -2,11 +2,13 @@ package com.maplemetric.character.presentation.code;
 
 import com.maplemetric.global.SuccessCode;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CharacterSuccessCode implements SuccessCode {
 
-    CHARACTER_READ_SUCCESS("CHARACTER_READ_SUCCESS", "캐릭터 조회에 성공했습니다.");
+    CHARACTER_READ_SUCCESS("CHARACTER_READ_SUCCESS", "캐릭터 조회에 성공했습니다."),
+    GET_CHARACTER_BASIC_SUCCESS("CHARACTER_BASIC_200", "캐릭터 기본 정보 조회에 성공했습니다.");
 
     private final String code;
     private final String message;
