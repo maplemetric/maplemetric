643dbf09ab807ffa1826d3c92fc29c4a26403ac1
jihoon <git4ub0@gmail.com>
2026-07-21 00:18:05 +0900

    feat: 넥슨 랭킹과 무릉 정보 조회 추가

---
+++ src/main/java/com/maplemetric/character/infrastructure/client/dto/CharacterDojangResponse.java
@@ -0,0 +1,10 @@
+package com.maplemetric.character.infrastructure.client.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record CharacterDojangResponse(
+        Integer dojangBestFloor
+) {
+}
