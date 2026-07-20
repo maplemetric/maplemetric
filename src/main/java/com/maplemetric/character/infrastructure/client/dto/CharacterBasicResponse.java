335bf3965f7f6577948d49c7a0d9cc714ec2e940
jihoon <git4ub0@gmail.com>
2026-07-20 13:18:05 +0900

    feat: 캐릭터 기본 정보 응답 DTO 추가

---
+++ src/main/java/com/maplemetric/character/infrastructure/client/dto/CharacterBasicResponse.java
@@ -0,0 +1,22 @@
+package com.maplemetric.character.infrastructure.client.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record CharacterBasicResponse(
+        String data,
+        String characterName,
+        String worldName,
+        String characterGender,
+        String characterClass,
+        String characterClassLevel,
+        Long characterExp,
+        String characterExpRate,
+        String characterGuildName,
+        String characterImage,
+        String characterDateCreate,
+        String accessFlag,
+        String liberationQuestClearFlag
+) {
+}
