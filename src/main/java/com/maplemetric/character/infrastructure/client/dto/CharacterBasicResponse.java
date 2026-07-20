a5aa3dc9f6f9c24865b0b5ac5d73caec9be6e6ca
jihoon <git4ub0@gmail.com>
2026-07-20 13:24:14 +0900

    feat: 캐릭터 기본 정보 응답 DTO 추가

--- src/main/java/com/maplemetric/character/infrastructure/client/dto/CharacterBasicResponse.java
+++ src/main/java/com/maplemetric/character/infrastructure/client/dto/CharacterBasicResponse.java
@@ -11,6 +11,7 @@ public record CharacterBasicResponse(
         String characterGender,
         String characterClass,
         String characterClassLevel,
+        Integer characterLever,
         Long characterExp,
         String characterExpRate,
         String characterGuildName,
