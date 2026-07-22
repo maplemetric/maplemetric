3276004c76a79bbfdf7de8b2355e455c4ff07d6a
jihoon <git4ub0@gmail.com>
2026-07-22 18:36:50 +0900

    test: Spring Modulith 모듈 경계 검증

---
+++ src/main/java/com/maplemetric/ranking/package-info.java
@@ -0,0 +1,9 @@
+@ApplicationModule(
+        allowedDependencies = {
+                "common",
+                "common::nexon"
+        }
+)
+package com.maplemetric.ranking;
+
+import org.springframework.modulith.ApplicationModule;
--- src/test/java/com/maplemetric/ModulithStructureTest.java
+++ src/test/java/com/maplemetric/ModulithStructureTest.java
@@ -1,14 +1,36 @@
 package com.maplemetric;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 
 class ModulithStructureTest {
 
+    private final ApplicationModules modules =
+            ApplicationModules.of(
+                    MaplemetricServiceApplication.class
+            );
+
     @Test
     void 모듈의존관계를검증한다() {
-        ApplicationModules.of(
-                MaplemetricServiceApplication.class
-        ).verify();
+        modules.verify();
+    }
+
+    @Test
+    void 공통Nexon인터페이스를공개한다() {
+        assertThat(
+                modules.getModuleByName("common")
+                        .orElseThrow()
+                        .getNamedInterfaces()
+                        .getByName("nexon")
+        ).isPresent();
+    }
+
+    @Test
+    void 기존Global모듈은존재하지않는다() {
+        assertThat(
+                modules.getModuleByName("global")
+        ).isEmpty();
     }
 }
