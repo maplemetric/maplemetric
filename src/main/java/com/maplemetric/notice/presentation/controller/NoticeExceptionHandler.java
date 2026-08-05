23864022c7d0db3335291cf7caff626c4ced094c
jihoon <git4ub0@gmail.com>
2026-08-05 21:42:17 +0900

    [BUG] 예상하지 못한 예외가 ApiResponse 계약을 지키지 않는다 (#193)
    * fix: 모듈 예외 Handler 우선순위를 명시
    
    * fix: 예상하지 못한 예외도 응답 계약을 지키게 함
    
    * test: 깨진 요청과 예상 못한 예외의 응답 계약 테스트 추가

--- src/main/java/com/maplemetric/notice/presentation/controller/NoticeExceptionHandler.java
+++ src/main/java/com/maplemetric/notice/presentation/controller/NoticeExceptionHandler.java
@@ -4,10 +4,13 @@ import com.maplemetric.common.ApiResponse;
 import com.maplemetric.notice.application.exception.NoticeException;
 import com.maplemetric.notice.domain.exception.InvalidNoticeCategoryException;
 import com.maplemetric.notice.presentation.code.NoticeErrorCode;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice(assignableTypes = NoticeController.class)
 public class NoticeExceptionHandler {
 
